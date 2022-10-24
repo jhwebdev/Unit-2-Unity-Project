e81300ef883ce4605667272b35d838611782f125
marcusmrno <113132281+marcusmrno@users.noreply.github.com>
2022-10-24 13:31:10 -0700

    new stuff
    added prism

--- UnityProject2/Assets/Scripts/ThirdPersonMovement.cs
+++ UnityProject2/Assets/Scripts/ThirdPersonMovement.cs
@@ -61,7 +61,6 @@ public class ThirdPersonMovement : MonoBehaviour
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);//smoothens the angle so we are not jumping from direction to direction
 
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
-
             //move
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDir.normalized * speed * Time.deltaTime);

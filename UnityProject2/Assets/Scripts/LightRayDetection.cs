b13133778396805f5bfc448e2a88de559cd7a463
marcusmrno <113132281+marcusmrno@users.noreply.github.com>
2022-11-03 11:19:35 -0700

    New Stuff fr
    eyayuh

---
+++ UnityProject2/Assets/Scripts/LightRayDetection.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightRayDetection : MonoBehaviour
+{
+    public GameObject character;
+    public ParticleSystem ps;
+
+    List<ParticleSystem.Particle> enter = new List<ParticleSystem.Particle>();
+
+    void onEnable()
+    {
+        ps = GetComponent<ParticleSystem>();
+    }
+
+    void OnParticleTrigger()
+    {
+        int triggered = ps.GetTriggerParticles(ParticleSystemTriggerEventType.Enter, enter);
+
+        if (triggered > 0)
+        {
+            character.transform.position += new Vector3(0, 20, 0);
+        }
+    }
+}
---
+++ UnityProject2/Assets/Scripts/Player Scripts/PickUpItems.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpItems : MonoBehaviour
+{
+    public GameObject player;
+    bool canPickup = false;
+    bool hasItem = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
